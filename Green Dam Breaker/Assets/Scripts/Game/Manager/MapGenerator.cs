b58e0595f36adfb7cb1d8bbb4af0877925333702
QIQIQI <beycome@163.com>
2017-07-04 18:45:10 +0800

    Fully accessible map

--- Green Dam Breaker/Assets/Scripts/Game/Manager/MapGenerator.cs
+++ Green Dam Breaker/Assets/Scripts/Game/Manager/MapGenerator.cs
@@ -6,10 +6,15 @@ public class MapGenerator : MonoBehaviour
 {
 	public GameObject tile;
 	public GameObject obstacle;
+	public float scaler = 1;
 	public int sizeX, sizeY;
+	public Coord startCoord;
+	public Coord[] testCoords;
 	public Transform tileParent;
 	[Range(0f, 1.0f)]
 	public float edgePercentage;
+	[Range(0f, 1.0f)]
+	public float obstaclePercentage;
 	public int obstaclePosRandomSeed;
 
 	private List<Coord> originalCoords;
@@ -18,6 +23,15 @@ public class MapGenerator : MonoBehaviour
 	void Start()
 	{
 		GenerateMap();
+		LogOut();
+	}
+
+	void LogOut()
+	{
+		foreach(Coord c in testCoords)
+		{
+			Debug.Log(c.ToString() + " gethascode : " + c.GetHashCode());
+		}
 	}
 
 	public void GenerateMap()
@@ -32,20 +46,84 @@ public class MapGenerator : MonoBehaviour
 			{
 				Vector3 pos = CoordToPosition(i,j);
 				GameObject newTile = Instantiate(tile, pos, Quaternion.Euler(Vector3.right * 90f));
+				newTile.transform.localScale *= scaler;
 				newTile.transform.localScale *= (1 - edgePercentage);
 				newTile.transform.SetParent(tileParent);
 			}
 		}
 
 		//generate obstacles
-		for(int i = 0; i < 10; i++)
+		int obstacleNum = (int)(sizeX * sizeY * obstaclePercentage);
+		int currentObstacleCount = 0;
+		bool[,] obstacleMap = new bool[sizeX, sizeY];	//mark if a coordinate stands an obstacle
+
+		for(int i = 0; i < obstacleNum; i++)
 		{
+			//before start, pretend to spawn this obstacle, and then check validity by the accessible algorithm
 			Coord c = GetRandomCoord();
-			Vector3 pos = CoordToPosition(c.x, c.y);
-			GameObject obs = Instantiate(obstacle, pos, Quaternion.identity);
-			obs.transform.position += Vector3.up * 0.5f;
-			obs.transform.SetParent(tileParent);
+			obstacleMap[c.x, c.y] = true;
+			currentObstacleCount++;
+
+			if(c != startCoord && IsMapFullyAccessible(obstacleMap, currentObstacleCount) == true)	//after check this obstacle is indeed OK to spawn
+			{
+				Vector3 pos = CoordToPosition(c.x, c.y);
+				GameObject obs = Instantiate(obstacle, pos, Quaternion.identity);
+				obs.transform.localScale *= scaler;
+				obs.transform.position += Vector3.up * 0.5f * scaler;
+				obs.transform.SetParent(tileParent);
+			}
+			else   //after check, this obstacle turns to make map inaccessible, cancel spawning
+			{
+				obstacleMap[c.x, c.y] = false;
+				currentObstacleCount--;
+			}
 		}
+	}
+
+	//Next time when you want to understand this method, try work it out in a tic-tac-toe 9-grid map
+	//this method exucetes one time when each obstacle is trying to spawn
+	bool IsMapFullyAccessible(bool[,] obstacleMap, int currentObstacleCount)
+	{
+		bool[,] checkBook = new bool[obstacleMap.GetLength(0), obstacleMap.GetLength(1)];	//mark if a coordinate has already been looked
+
+		Queue<Coord> envaluationQueue = new Queue<Coord>();	//when coord is in queque, it has been looked and it must be a path tile. After check its neighbours it is removed from queue
+
+		int calculatedPathTileCount = 1;	//1 since startCoord is always path tile
+
+		envaluationQueue.Enqueue(startCoord);	//always from this coordinate, and start a radiation style check(pretty much like A* path finding)
+		checkBook[startCoord.x, startCoord.y] = true;
+
+		while(envaluationQueue.Count > 0)
+		{
+			Coord thisCoord = envaluationQueue.Dequeue();
+
+			for(int x = -1; x < 2; x++)
+			{
+				for(int y = -1; y < 2; y++)	//loop through the 8 neighboours of current coordinate
+				{
+					Coord neighbour = new Coord(thisCoord.x + x, thisCoord.y + y);
+
+					if(x != 0 && y != 0)	//ignore the 4 diagonal coord, because 4 diagonal obstacles won't block map path
+						continue;
+
+					if(neighbour.x < 0 || neighbour.x > obstacleMap.GetLength(0) - 1 || neighbour.y < 0 || neighbour.y > obstacleMap.GetLength(1) - 1)	//check if the neighbour coord is not in bound of map
+						continue;
+					
+					if(checkBook[neighbour.x, neighbour.y] == true)	//check if this coord has already been looked
+						continue;
+
+					if(obstacleMap[neighbour.x, neighbour.y] == false)	//there is no obstacle at this coord
+					{
+						calculatedPathTileCount++;
+						envaluationQueue.Enqueue(neighbour);	//within next several while loop, this neight will be envaluated as thisCoord
+						checkBook[neighbour.x, neighbour.y] = true;
+					}
+				}
+			}
+		}
+
+		int targetPathTileCount = sizeX * sizeY - currentObstacleCount;
+		return calculatedPathTileCount == targetPathTileCount;
 	}
 
 	public void ClearMap()
@@ -86,11 +164,12 @@ public class MapGenerator : MonoBehaviour
 	///up direction is not calculated and will always be 0
 	Vector3 CoordToPosition(int x, int y)
 	{
-		Vector3 pos = new Vector3(-sizeX/2 + x + 0.5f, 0f, -sizeY/2 + y + 0.5f);
+		Vector3 pos = new Vector3(-sizeX/2 + (x + 0.5f) * scaler, 0f, -sizeY/2 + (y + 0.5f) * scaler);
 		return pos;
 	}
 }
 
+[System.Serializable]
 public struct Coord
 {
 	public int x;
@@ -100,5 +179,31 @@ public struct Coord
 	{
 		x = _x;
 		y = _y;
+	}
+
+	public static bool operator ==(Coord a, Coord b)
+	{
+		return a.x == b.x && a.y == b.y;
+	}
+
+	public static bool operator !=(Coord a, Coord b)
+	{
+		return !(a == b);
+	}
+
+	public override bool Equals (object o)
+	{
+		Coord c = (Coord)o;
+		return this == c;
+	}
+
+	public override int GetHashCode ()	//TODO what is the hash code return of a struct?
+	{
+		return base.GetHashCode();
+	}
+
+	public override string ToString ()
+	{
+		return string.Format ("[Coord({0},{1})]", x, y);
 	}
 }
