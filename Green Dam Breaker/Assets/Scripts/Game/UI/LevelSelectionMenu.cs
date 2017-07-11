69ccc11c45ad5647cc95b21470c25cc353dbc930
QIQIQI <beycome@163.com>
2017-07-11 18:42:49 +0800

    Navigation Menu Style

---
+++ Green Dam Breaker/Assets/Scripts/Game/UI/LevelSelectionMenu.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSelectionMenu : MonoBehaviour 
+{
+	public ModeContentGroup[] contents;
+
+	private bool isScrolling = false;
+
+	void Start()
+	{
+		isScrolling = false;
+	}
+
+	public void ScrollRight(int contentIndex)
+	{
+		if(isScrolling)
+			return;
+
+		int dir = 1;
+
+		if(contents[contentIndex].pivot + dir > contents[contentIndex].childs.Length - 1)
+			return;
+
+		contents[contentIndex].pivot += dir;
+
+		StartCoroutine(ScrollCo(contentIndex, dir));
+
+	}
+
+	public void ScrollLeft(int contentIndex)
+	{
+		if(isScrolling)
+			return;
+
+		int dir = -1;
+
+		if(contents[contentIndex].pivot + dir < 0)
+			return;
+
+		contents[contentIndex].pivot += dir;
+
+		StartCoroutine(ScrollCo(contentIndex, dir));
+	}
+
+	IEnumerator ScrollCo(int contentIndex, int dir)
+	{
+		isScrolling = true;
+
+		float percent = 0f;
+		ModeContentGroup mcg = contents[contentIndex];
+
+		while(percent < 1f)
+		{
+			float fromX = mcg.startX - (mcg.pivot - dir) * mcg.scrollAmount;
+			float toX = mcg.startX - mcg.pivot * mcg.scrollAmount;
+			float newX = Mathf.Lerp(fromX, toX, percent);
+			mcg.content.localPosition = new Vector3(newX, mcg.content.localPosition.y, mcg.content.localPosition.z);
+
+			percent += Time.deltaTime * mcg.scrollSpeed;
+			yield return null;
+		}
+
+		mcg.content.localPosition = new Vector3(mcg.startX - mcg.pivot * mcg.scrollAmount, mcg.content.localPosition.y, mcg.content.localPosition.z);
+
+		isScrolling = false;
+	}
+}
+
+[System.Serializable]
+public class ModeContentGroup
+{
+	public RectTransform content;
+	public float startX;	//the start Xpos
+	public RectTransform[] childs;
+	public float scrollAmount;	//how much Xpos is scroll from one card to the next
+	public float scrollSpeed = 2f;
+
+	public float endX {
+		get { 
+			return startX - scrollAmount * (childs.Length - 1);
+		}
+	}
+
+	public int pivot = 0;	//which card is currently at
+}
---
+++ Green Dam Breaker/Assets/Scripts/Game/UI/NavigationMenuManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavigationMenuManager : MonoBehaviour 
+{
+	public CanvasGroup[] allPanels;
+
+	void Start()
+	{
+		if(allPanels.Length <= 0)
+		{
+			Debug.LogWarning("Dont forget to set up menu panels in inspector.");
+			return;
+		}
+
+		DeactiveAllPanels();
+		ActivePanel(0);
+	}
+
+	public void ActivePanel(int index)
+	{
+		allPanels[index].alpha = 1;
+		allPanels[index].gameObject.SetActive(true);
+	}
+
+	public void DeactiveAllPanels()
+	{
+		foreach(CanvasGroup cg in allPanels)
+		{
+			cg.alpha = 0f;
+			cg.gameObject.SetActive(false);
+		}
+	}
+
+	public void LoadScene(string sceneName)
+	{
+		if(SceneController.Instance == null)
+		{
+			Debug.LogWarning("Game should start at Persistent scene, check if you have persistent scene loaded.");
+		}
+		SceneController.Instance.MyLoadScene(sceneName);
+	}
+}
