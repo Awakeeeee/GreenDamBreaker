78e2353cb648b6f440e124513f5b2b3d51dc704a
QIQIQI <beycome@163.com>
2017-07-26 18:04:56 +0800

    Add in 2 camera shake tools

---
+++ Green Dam Breaker/Assets/Scripts/Tools/CameraMonoShake.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMonoShake : MonoBehaviour
+{
+    public float shakeAmount = 2.0f;
+    public float onewayTime = 0.05f;
+
+    private Transform shakeObj;
+	private Vector3 originalPos;
+    private bool goShake;
+    private bool isShaking;
+
+    void Start()
+    {
+		//this should be Main Camera
+		shakeObj = this.transform;
+		originalPos = this.transform.position;
+
+        goShake = false;
+        isShaking = false;
+    }
+
+    void Update()
+    {
+//        if (Input.GetKeyDown(KeyCode.J))
+//        {
+//            TriggerShake();
+//        }
+
+        if (!goShake)
+            return;
+
+        if(!isShaking)
+            StartCoroutine(MonoShake());
+    }
+
+    IEnumerator MonoShake()
+    {
+        isShaking = true;
+
+		Vector3 shakeDir = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, -1f), Random.Range(-1f, 1f)).normalized;
+		Vector3 destination = originalPos + shakeDir * shakeAmount;
+        float timer = 0.0f;
+
+        while (timer < onewayTime)
+        {
+            shakeObj.localPosition = Vector3.Lerp(originalPos, destination, timer / onewayTime);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        timer = 0.0f;
+
+        while (timer < onewayTime)
+        {
+            shakeObj.localPosition = Vector3.Lerp(destination, originalPos, timer / onewayTime);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        shakeObj.localPosition = originalPos;
+
+        goShake = false;
+        isShaking = false;
+    }
+
+	//External connect point
+    public void TriggerShake()
+    {
+        goShake = true;
+    }
+}
---
+++ Green Dam Breaker/Assets/Scripts/Tools/CameraShaker.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShaker : MonoBehaviour
+{
+    [Header("Random Shake Value Monitor")]
+    public float randomAmount;
+    public float randomtime;
+    public float randomDecayFactor = 1.0f;
+
+    private Transform shakeObj;
+    private Vector3 originalPos;
+    private bool goShake;
+
+    void Start()
+    {
+		//most time this should be the camera
+		shakeObj = this.transform;
+		originalPos = this.transform.position;
+    }
+
+    void OnEnable()
+    {
+        goShake = false;
+    }
+
+    void Update()
+    {
+//        if (Input.GetKeyDown(KeyCode.J))
+//        {
+//            TriggerRandomShake(0.5f, 0.05f, 1.0f);
+//        }
+
+        if (!goShake)
+            return;
+
+        if (randomtime > 0f)
+        {
+            Vector3 shakeAdd = Random.insideUnitSphere * randomAmount;
+            shakeObj.localPosition = originalPos + shakeAdd;
+            randomtime -= Time.deltaTime * randomDecayFactor;
+        }
+        else
+        {
+            shakeObj.localPosition = originalPos;
+            randomtime = 0f;
+            goShake = false;
+        }
+
+    }
+
+	public void TriggerRandomShake(float _amount = 0.5f, float _time = 0.05f, float _decay = 1.0f)
+    {
+        goShake = true;
+        randomAmount = _amount;
+        randomtime = _time;
+        randomDecayFactor = _decay;
+    }
+}
